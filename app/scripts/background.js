88aae5ee24ab3eef511f6c1d6c85efdb79cee4c2
b3x0b <b3x0b@ugrad.cs.ubc.ca>
2019-03-16 16:58:36 +0000

    some new stuff

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,9 +1,94 @@
+let id = "NA";
+
+let currentLat = 0;
+let currentLon = 0;
+
+const SERVER_ADDRESS = "https://btogether.herokuapp.com/api/";
+
+fetch(SERVER_ADDRESS + "down/id")
+  .then(function(response) {
+    id = response.json()['id'];
+  });
+
+browser.runtime.onMessage.addListener(notify);
+
 browser.runtime.onInstalled.addListener((details) => {
   console.log('previousVersion', details.previousVersion)
 })
 
-browser.browserAction.setBadgeText({
-  text: `'Allo`
-})
+function notify(message, sender, sendResponse) {
+  switch(message.intent){
+    case 'getId':
+      sendResponse({intent: 'id', id: id});
+      break;
+    case 'notify':
+      createNotify(message.title, message.content);
+      break;
+    case 'sendMetric':
+      sendMetric(message);
+      break;
+  }
+}
+
+function sendMetric(message) {
+  fetch(SERVER_ADDRESS + "up/" + message.name,
+    {
+      method: "POST",
+      body: prepBody(message)
+    })
+    .then(function(res){  })
+}
+
+function createNotify(title, content){
+  browser.notifications.create({
+    "type": "basic",
+    "iconUrl": browser.extension.getURL("link.png"),
+    "title": title,
+    "message": content
+  });
+}
+
+function execServerCommand(message){
+  switch (message.type) {
+    case 'notify':
+      createNotify(message.title, message.content);
+      break;
+  }
+  console.log(message);
+}
+
+setInterval(function () {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    console.log(position.coords.latitude, position.coords.longitude);
+    if(position.coords.latitude !== currentLat || position.coords.longitude !== currentLon){
+      currentLat = position.coords.latitude;
+      currentLon = position.coords.longitude;
+      console.log(currentLat, currentLon);
+      fetch(SERVER_ADDRESS + "up/loc",
+        {
+          method: "POST",
+          body: prepBody({
+            lat:  currentLat,
+            lon: currentLon
+          })
+        })
+        .then(function(res){  })
+    }
+  });
+
+  fetch(SERVER_ADDRESS + "down/control")
+    .then(function(response) {
+      execServerCommand(JSON.parse(response));
+    });
+
+
+
+}, 3000);
+
+
+function prepBody(body){
+  body.id = id;
+  return JSON.stringify(body);
+}
 
 console.log(`'Allo 'Allo! Event Page for Browser Action`)
--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,1 +1,30 @@
-console.log(`'Allo 'Allo! Content script`)
+browser.runtime.sendMessage({"intent": 'sendMetric', 'name': 'open', 'url': window.location.hostname});
+
+window.onbeforeunload = function() {
+  browser.runtime.sendMessage({"intent": 'sendMetric', 'name': 'close', 'url': window.location.hostname});
+}
+
+// navigator.mediaDevices.getUserMedia({ audio: true })
+//   .then(stream => {
+//     const mediaRecorder = new MediaRecorder(stream);
+//     mediaRecorder.start();
+//
+//     const audioChunks = [];
+//     mediaRecorder.addEventListener("dataavailable", event => {
+//       audioChunks.push(event.data);
+//     });
+//
+//     mediaRecorder.addEventListener("stop", () => {
+//       const audioBlob = new Blob(audioChunks);
+//       const audioUrl = URL.createObjectURL(audioBlob);
+//       const audio = new Audio(audioUrl);
+//       audio.play();
+//       console.log("best case!!!!!!!!!!!");
+//     });
+//
+//     setTimeout(() => {
+//       mediaRecorder.stop();
+//     }, 3000);
+//   }).catch(function (err) {
+//   console.log(err);
+// });
